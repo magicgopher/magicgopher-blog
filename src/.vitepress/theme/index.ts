4a5c68cc9ccf21e71dec11ee33f0edd0b24e7e17
matrix <gopher997@gmail.com>
2024-08-22 04:14:56 +0800

    style: 添加浏览器特定类以调整样式。

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,6 +1,5 @@
 import { h } from 'vue';
-import { inBrowser } from 'vitepress';
-import type { Theme } from 'vitepress';
+import { inBrowser, Theme } from 'vitepress';
 import escookTheme from '@escook/vitepress-theme';
 import BackTop from '@/components/BackTop.vue';
 import VisitorStats from '@/components/VisitorStats.vue';
@@ -27,4 +26,19 @@ export default {
             }
         }
     }
-} satisfies Theme
+} satisfies Theme;
+
+// 检查代码是否在浏览器环境中运行(而不是在服务器端环境如 Node.js 中运行)
+if (typeof window !== 'undefined') {
+    // 通过检查用户代理字符串来检测用户的浏览器类型
+    const browser = navigator.userAgent.toLowerCase()
+    // 如果用户代理字符串包含 'chrome'，为 HTML 根元素添加 'browser-chrome' 类
+    if (browser.includes('chrome'))
+        document.documentElement.classList.add('browser-chrome')
+    // 如果用户代理字符串包含 'firefox'，为 HTML 根元素添加 'browser-firefox' 类
+    else if (browser.includes('firefox'))
+        document.documentElement.classList.add('browser-firefox')
+    // 如果用户代理字符串包含 'safari'，为 HTML 根元素添加 'browser-safari' 类
+    else if (browser.includes('safari'))
+        document.documentElement.classList.add('browser-safari')
+}
