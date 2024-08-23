4454a323f014de2a9ece224d2ed491882f0a9914
MagicGopher <gopher997@gmail.com>
2024-08-23 18:07:43 +0800

    chore: 评论区组件代码格式调整。

--- src/.vitepress/components/GiscusComment.vue
+++ src/.vitepress/components/GiscusComment.vue
@@ -1,20 +1,27 @@
 <template>
     <div style="margin-top: 24px">
-        <Giscus id="comments" repo="MagicGopher/MagicGopher-Blog" repoid="R_kgDOMNveng" category="General"
-            categoryid="DIC_kwDOMNvens4ChhfK" mapping="title" term="Welcome to giscus!" reactionsenabled="1"
-            emitmetadata="0" inputposition="top" loading="lazy" :theme="isDark ? 'dark' : 'light'" :key="fullPath">
-        </Giscus>
+        <Giscus
+            id="comments"
+            repo="MagicGopher/MagicGopher-Blog"
+            repoid="R_kgDOMNveng"
+            category="General"
+            categoryid="DIC_kwDOMNvens4ChhfK"
+            mapping="title"
+            term="Welcome to giscus!"
+            reactionsenabled="1"
+            emitmetadata="0"
+            inputposition="top"
+            loading="lazy"
+            :theme="isDark ? 'dark' : 'light'"
+            :key="route.path"
+        ></Giscus>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import Giscus from '@giscus/vue';
 import { useRoute, useData } from 'vitepress';
-import { computed } from 'vue';
 
 const route = useRoute();
 const { isDark } = useData();
-
-// 计算fullPath属性
-const fullPath = computed(() => route.path);
 </script>
